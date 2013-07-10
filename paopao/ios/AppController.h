8cd37589fc1b9c001d506b4c19461d9df038b481
woshinyc <alvenyuan@limatoMacBook-Pro.local>
2013-07-10 10:25:31 +0800

    Initial Commit

---
+++ paopao/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  paopaoAppController.h
+//  paopao
+//
+//  Created by NycMac on 13-7-10.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ paopao/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  paopaoAppController.h
+//  paopao
+//
+//  Created by NycMac on 13-7-10.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
